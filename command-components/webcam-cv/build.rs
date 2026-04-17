f8983acf23c62fd1b430d5a56ea927fa67be4de4
Sibren Wieme <Sibren.Wieme@UGent.be>
2026-04-17 08:40:45 +0200

    chore(workspace): remove webcam-cv and yolo-detector-terminal, update Justfile and deps

--- command-components/webcam-cv/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let target = std::env::var("TARGET").unwrap_or_default();
-    if target == "wasm32-wasip2" {
-        // Update this path to your own sysroot locatie
-        // let sysroot = "/Users/sibrenwieme/Documents/Masterproef/usb-wasm/rusb-wasi/examples/wasi-workload/wasi-sysroot";
-        // println!("cargo:rustc-link-arg={}/usr/lib/cguest_component_type.o", sysroot);
-    }
-}
